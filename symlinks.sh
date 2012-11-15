cc86d5528a129a46937a42abcacd4b2937e7c7ae
Collin Brooks <collin.brooks@gmail.com>
2012-11-15 12:37:33 -0600

    Added ~/bin to the path and modified the symlinks.sh file.

--- symlinks.sh
+++ symlinks.sh
@@ -2,9 +2,15 @@
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 cd "$(dirname "$0")"
 dir=`pwd`
-echo $dir
+
+function symlink() {
+    echo $1 => $2
+    ln -sF $1 $2
+}
+
 #This script is responsible for creating/updating any symlinks to be
 #made in the home directory upon running bootstrap.sh
-ln -sfF ${dir}/.vim ~/.vim
-ln -sfF ${dir}/.vimrc ~/.vimrc
-ln -sfF ${dir}/.irssi ~/.irssi
+
+symlink ${dir}/.vim/ ~/.vim
+symlink ${dir}/.vimrc ~/.vimrc
+#symlink ${dir}/.irssi/ ~/.irssi
