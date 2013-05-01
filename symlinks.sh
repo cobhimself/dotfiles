bf3325e71e7cbdfd7f4b865ab0242a0e4f4bf42f
Collin Brooks <collin.brooks@gmail.com>
2013-04-30 21:07:33 -0500

    Fix symlinks script.

--- symlinks.sh
+++ symlinks.sh
@@ -4,14 +4,13 @@ cd "$(dirname "$0")"
 dir=`pwd`
 
 function symlink() {
-    echo $1 => $2
-    ln -sfF $1 $2
+    ln -sfFv $1 $2
 }
 
 #This script is responsible for creating/updating any symlinks to be
 #made in the home directory upon running bootstrap.sh
 
-symlink ${dir}/.vim/ ~/.vim
-symlink ${dir}/.vimrc ~/.vimrc
-symlink ${dir}/.multitail.conf ~/.multitail.conf
+#symlink ${dir}/.vim/ ~/.vim
+symlink ${dir}/.vim/.vimrc ~/.vimrc
+symlink ${dir}/.vim/.gvimrc ~/.gvimrc
 #symlink ${dir}/.irssi/ ~/.irssi
